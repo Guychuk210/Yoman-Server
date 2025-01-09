5947fd1a114cabe3dca448daaf8bd4ff9f9348c0
Guy Levy <guy.levy01@post.runi.ac.il>
2025-01-09 19:24:22 +0200

    Revert "fixes"
    This reverts commit ffdcb5c2e072cebc55216635c3efd36ccf9387ce.

--- src/index.ts
+++ src/index.ts
@@ -142,12 +142,12 @@ app.post('/generate-diary', async (req: Request, res: Response) => {
     const { text, style, assistantId, entryId, userId } = req.body;
     
     console.log('=== Generate Diary Request ===');
-    console.log('AssistantId received:', assistantId);
     console.log('Style:', style);
+    console.log('Text length:', text?.length);
     console.log('EntryId:', entryId);
     console.log('UserId:', userId);
 
-    // If no assistantId is provided, create a new assistant and save it
+    // If no assistantId is provided, create a new assistant
     let currentAssistantId = assistantId;
     if (!currentAssistantId) {
       console.log('Creating new assistant...');
@@ -160,14 +160,7 @@ app.post('/generate-diary', async (req: Request, res: Response) => {
         tools: []
       });
       currentAssistantId = assistant.id;
-      
-      // Save the new assistant ID to the user's document
-      const userRef = db.collection('users').doc(userId);
-      await userRef.update({
-        assistantId: currentAssistantId
-      });
-      
-      console.log('New assistant created and saved:', currentAssistantId);
+      console.log('New assistant created:', currentAssistantId);
     }
 
     // Create thread and add initial message with style-specific instructions
